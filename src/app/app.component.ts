8f6e9c3c3f35088146d1f4e6ff4618f17b68aead
NicoleCondeDuque <nicolecondeduque@gmail.com>
2024-11-22 16:12:47 -0700

    welcome section done, with style.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { WelcomeComponent  } from './components/welcome/welcome.component'
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
